ba41bcdda17d5b7759627827e19f3d9bfba1f976
shs747@gmail.com <shin@Solitudeeui-MacBook-Pro.local>
2017-04-16 18:35:14 +0900

    유니티 애드가 제대로 테스트 되지 않음. 구글 결제도 계속해서 인증해야한다고 뜬다

--- Unity/Assets/Script/ShopManager.cs
+++ Unity/Assets/Script/ShopManager.cs
@@ -1,13 +1,18 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 using UnityEngine.SceneManagement;
+using UnityEngine.Purchasing;
+using UnityEngine.Advertisements;
 
-public class ShopManager : MonoBehaviour {
+public class ShopManager : MonoBehaviour, IStoreListener {
     public static ShopManager Ins;
 
     public Sprite hintItemIcon;
     public Sprite timerItemIcon;
+
+    public GameObject rewardAdButton;
 
     /// <summary>
     /// Awake is called when the script instance is being loaded.
@@ -22,7 +27,14 @@ public class ShopManager : MonoBehaviour {
     public enum ITEM { NONE=0, HINT, INC_TIMER };
 	void Start ()
     {
-		NetworkManager.Ins.GetItemInfoList();
+		//NetworkManager.Ins.GetItemInfoList();
+        // iap 초기화
+        InitializePurchasing();
+
+        if (PlayerPrefs.GetInt(System.DateTime.Now.ToString(), 0) >= 3)
+        {
+            rewardAdButton.GetComponent<Button>().interactable = false;
+        }
 	}
 
 	void Update ()
@@ -83,5 +95,169 @@ public class ShopManager : MonoBehaviour {
     public void add_increase_timer_x50_btn_clicked()
     {
         Debug.Log("타이머 증가 50개 추가 버튼 클릭");
+    }
+
+    private static IStoreController storeController;
+    private static IExtensionProvider extensionProvider;
+
+    public const string productId1 = "test_timer_item_10";
+
+    private bool IsInitialized()
+    {
+        return (storeController != null && extensionProvider != null);
+    }
+        
+    public void InitializePurchasing()
+    {
+        if (IsInitialized())
+            return;
+
+        var module = StandardPurchasingModule.Instance();
+
+        ConfigurationBuilder builder = ConfigurationBuilder.Instance(module);
+
+        builder.AddProduct(productId1, ProductType.Consumable, new IDs
+        {
+           // { productId1, AppleAppStore.Name },
+            { productId1, GooglePlay.Name },
+        });
+
+        UnityPurchasing.Initialize(this, builder);
+    }
+
+    public void BuyProductID(string productId)
+    {
+        try
+        {
+            if (IsInitialized())
+            {
+                Product p = storeController.products.WithID(productId);
+
+                if (p != null && p.availableToPurchase)
+                {
+                    Debug.Log(string.Format("Purchasing product asychronously: '{0}'", p.definition.id));
+                    storeController.InitiatePurchase(p);
+                }
+                else
+                {
+                    Debug.Log("BuyProductID: FAIL. Not purchasing product, either is not found or is not available for purchase");
+                }
+            }
+            else
+            {
+                Debug.Log("BuyProductID FAIL. Not initialized.");
+            }
+        }
+        catch (UnityException e)
+        {
+            Debug.Log("BuyProductID: FAIL. Exception during purchase. " + e);
+        }
+    }
+
+    public void OnInitialized(IStoreController sc, IExtensionProvider ep)
+    {
+        Debug.Log("OnInitialized : PASS");
+
+        storeController = sc;
+        extensionProvider = ep;
+    }
+
+    public void OnInitializeFailed(InitializationFailureReason reason)
+    {
+        Debug.Log("OnInitializeFailed InitializationFailureReason:" + reason);
+    }
+
+    public PurchaseProcessingResult ProcessPurchase(PurchaseEventArgs args)
+    {
+        Debug.Log(string.Format("ProcessPurchase: PASS. Product: '{0}'", args.purchasedProduct.definition.id));
+
+        if (args.purchasedProduct.definition.id == productId1)
+        {
+            NetworkManager.Ins.PurchaseItemRequest(productId1);
+        }
+        else
+        {
+
+        }
+
+        return PurchaseProcessingResult.Complete;
+    }
+
+    public void OnPurchaseFailed(Product product, PurchaseFailureReason failureReason)
+    {
+        Debug.Log(string.Format("OnPurchaseFailed: FAIL. Product: '{0}', PurchaseFailureReason: {1}", product.definition.storeSpecificId, failureReason));
+    }
+
+
+    public void OnTestTimerItemButton()
+    {
+        BuyProductID(productId1);
+        Debug.Log("구매구매");
+    }
+
+    /*
+    public void RestorePurchase()
+    {
+        if (!IsInitialized())
+        {
+            Debug.Log("RestorePurchases FAIL. Not initialized.");
+            return;
+        }
+
+        if (Application.platform == RuntimePlatform.IPhonePlayer || Application.platform == RuntimePlatform.OSXPlayer)
+        {
+            Debug.Log("RestorePurchases started ...");
+
+            var apple = extensionProvider.GetExtension<IAppleExtensions>();
+
+            apple.RestoreTransactions
+                (
+                    (result) => { Debug.Log("RestorePurchases continuing: " + result + ". If no further messages, no purchases available to restore."); }
+                );
+        }
+        else
+        {
+            Debug.Log("RestorePurchases FAIL. Not supported on this platform. Current = " + Application.platform);
+        }
+    }
+    */
+
+    public void OnClickedRewadAdButton()
+    {
+        if (PlayerPrefs.GetInt(System.DateTime.Now.ToString(), 0) < 3)
+        {
+            ShowRewardedAd();
+        }
+    }
+
+    public void ShowRewardedAd()
+    {
+        if (Advertisement.IsReady("rewardedVideo"))
+        {
+            var options = new ShowOptions { resultCallback = HandleShowResult };
+            Advertisement.Show("rewardedVideo", options);
+        }
+    }
+
+    private void HandleShowResult(ShowResult result)
+    {
+        switch (result)
+        {
+            case ShowResult.Finished:
+                Debug.Log("The ad was successfully shown.");
+                //
+                // YOUR CODE TO REWARD THE GAMER
+                // Give coins etc.
+                int adCount = PlayerPrefs.GetInt(System.DateTime.Now.ToString(), 0);
+                adCount++;
+                PlayerPrefs.SetInt(System.DateTime.Now.ToString(), adCount);
+                break;
+            case ShowResult.Skipped:
+                Debug.Log("The ad was skipped before reaching the end.");
+                break;
+            case ShowResult.Failed:
+                Debug.LogError("The ad failed to be shown.");
+                break;
+        }
     }
 }
